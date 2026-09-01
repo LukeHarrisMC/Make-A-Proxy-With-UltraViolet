da7d3ddcb2f9dc90a67bc2e6364b4d6d95302e7a
LukeHarrisMC <mrlukeharris5@gmail.com>
2026-09-01 15:56:17 +1000

    Add script to start Ultraviolet proxy server

---
+++ startproxyserver.sh
@@ -0,0 +1,6 @@
+echo "Downloading Ultraviolet..."
+git clone https://github.com/titaniumnetwork-dev/Ultraviolet-App.git
+echo "Installing Node Package Manager"
+npm install
+echo "Starting Up UltraViolet"
+npm start
